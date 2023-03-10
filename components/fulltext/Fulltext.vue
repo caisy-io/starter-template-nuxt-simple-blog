563ac4a0c8f9cabe934a18cdf12dd5327f88a693
bfloku <abso.floku@gmail.com>
2023-03-10 09:40:41 -0300

    feat: added fulltext component

---
+++ components/fulltext/Fulltext.vue
@@ -0,0 +1,21 @@
+<template>
+  <CenterContainer>
+    <article class="prose lg:prose-xl">
+      <RichtextRenderer
+        v-if="text?.json"
+        :overwrites="{ documentLink: DocumentLink }"
+        :connections="{text?.connections}"
+        :node="{text?.json}"
+      />
+    </article>
+  </CenterContainer>
+</template>
+
+<script setup>
+import { RichtextRenderer } from "@caisy/rich-text-vue-renderer";
+const props = defineProps({
+  text: {
+    json: Object,
+  },
+});
+</script>
---
+++ components/fulltext/overwrites/DocumentLink.vue
@@ -0,0 +1,18 @@
+<template>
+  <Asset
+    v-for="component in connections"
+    v-if="
+      component?.__typename == 'Asset' &&
+      node?.attrs?.documentId == component.id
+    "
+    :src="component?.src"
+    :description="component?.description"
+  />
+</template>
+
+<script setup>
+const props = defineProps({
+  node: Object,
+  connections: [Object],
+});
+</script>
