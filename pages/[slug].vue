00906d644da0b54552966c0f4760a8cdfcf9d68b
bfloku <abso.floku@gmail.com>
2023-03-13 16:04:09 -0300

    Create [slug].vue

---
+++ pages/[slug].vue
@@ -0,0 +1,12 @@
+<template>
+  <Page v-bind="props" />
+</template>
+
+<script setup>
+import { getProps } from "~~/services/content/getProps";
+import Page from "~~/layouts/Page.vue";
+
+const route = useRoute();
+const { slug } = route.params;
+const props = await getProps({ slug });
+</script>
