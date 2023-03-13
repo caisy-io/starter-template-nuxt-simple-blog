8f13d1e4f7d3c0d46bd6b38d980b1928e8ebc2cc
bfloku <abso.floku@gmail.com>
2023-03-13 08:37:41 -0300

    feat: added footer component

---
+++ layouts/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex items-center justify-center">
+    <footer class="prose pb-10 pt-20 flex items-center justify-center">
+      <RichTextRenderer v-if="content?.json" :node="content?.json" />
+    </footer>
+  </div>
+</template>
+
+<script setup>
+import { RichTextRenderer } from "@caisy/rich-text-vue-renderer";
+const props = defineProps({
+  content,
+});
+</script>
