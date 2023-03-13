1dabf684615d2198039b76a77be30c0fb142a8e5
bfloku <abso.floku@gmail.com>
2023-03-13 09:25:42 -0300

    feat: added componentselector component

---
+++ layouts/ComponentSelector.vue
@@ -0,0 +1,28 @@
+<template>
+  <component
+    :is="COMPONENTS[componentType.__typename]"
+    v-bind="componentType"
+  ></component>
+</template>
+
+<script setup>
+import Headline from "~~/components/Headline.vue";
+import Fulltext from "~~/components/fulltext/Fulltext.vue";
+import BlogArticleGrid from "~~/components/blog-article-grid/BlogArticleGrid.vue";
+import NewsletterSignup from "~~/components/NewsletterSignup.vue";
+import FullwidthBlogTeaser from "~~/components/FullwidthBlogTeaser.vue";
+import ContactForm from "~~/components/ContactForm.vue";
+
+const COMPONENTS = {
+  Headline,
+  Fulltext,
+  BlogArticleGrid,
+  NewsletterSignup,
+  FullwidthBlogTeaser,
+  ContactForm,
+};
+
+const props = defineProps({
+  componentType,
+});
+</script>
