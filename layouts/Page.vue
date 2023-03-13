18a0d3c06d3869e8f3f6e3197eae3cc927770002
bfloku <abso.floku@gmail.com>
2023-03-13 09:30:54 -0300

    feat: added page component

---
+++ layouts/Page.vue
@@ -0,0 +1,21 @@
+<template>
+  <Layout
+    :seo="props?.Page?.seo"
+    :navigation="props?.Navigation"
+    :footer="props?.Footer"
+  >
+    <ComponentSelector
+      v-for="componentType in Page?.components"
+      :componentType="componentType"
+    />
+  </Layout>
+</template>
+
+<script setup>
+import ComponentSelector from "./ComponentSelector.vue";
+const props = defineProps({
+  Page,
+  Navigation,
+  Footer,
+});
+</script>
